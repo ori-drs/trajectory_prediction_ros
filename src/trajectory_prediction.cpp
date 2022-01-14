cd9ddc895bf80d34905692317f2fe2b806c3103b
Mark <mark.finean@stx.ox.ac.uk>
2022-01-14 13:29:29 +0000

    Correct package name

--- src/trajectory_prediction.cpp
+++ src/trajectory_prediction.cpp
@@ -1,5 +1,5 @@
 // -- BEGIN LICENSE BLOCK ----------------------------------------------
-// This file is part of the gpu_voxels_ros package.
+// This file is part of the trajectory_prediction_ros package.
 // © Copyright 2022, Mark Finean and Luka Petrovic 
 // 
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
